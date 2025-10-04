54281bb05dffa526dc828b576e1bd078f2ecc82f
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-10-04 11:18:51 -0700

    adding connection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,26 @@
+import amqp from "amqplib";
+import process from "node:process";
+
 async function main() {
-  console.log("Starting Peril server...");
+	console.log("Starting Peril server...");
+	const connectionString = "amqp://guest:guest@localhost:5672/";
+	const conn = await amqp.connect(connectionString);
+	console.log("Connection success");
+
+	const shutdown = async () => {
+		console.log("Shutting down...");
+		try {
+			await conn.close();
+		} catch (error) {
+			console.log("Error: ", error);
+			process.exit(0);
+		}
+	};
+	process.on("SIGINT", shutdown);
+	process.on("SIGTERM", shutdown);
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+	console.error("Fatal error:", err);
+	process.exit(1);
 });
